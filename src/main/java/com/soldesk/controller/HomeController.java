4066cc2fff1fa4ac845c3b543c27e1b8766316eb
Seungwoo Oh <sw6016@hanmail.net>
2023-07-12 09:24:58 +0900

    COMMIT

--- src/main/java/com/soldesk/controller/HomeController.java
+++ src/main/java/com/soldesk/controller/HomeController.java
@@ -13,9 +13,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	// 초기화면
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String goHome() {
+		return "homeVer1";
+	}
+	
+	// 로그인 화면
+	@RequestMapping(value = "/login/login", method = RequestMethod.GET)
+	public String goLogin() {
+		return "user/loginForm";
+	}
+	
+	// 회원가입 화면
+	@RequestMapping(value = "/login/signup", method = RequestMethod.GET)
+	public String goSignup() {
+		return "register/registerForm";
+	}
+	
+	// 전국 지도 화면
+	@RequestMapping(value = "/mountain/map", method = RequestMethod.GET)
+	public String goMountain_map() {
 		return "mountain/map";
+	}
+	
+	// 마이 페이지 [미구현]
+	@RequestMapping(value = "/user/mypage", method = RequestMethod.GET)
+	public String goMypage() {
+		return "homeVer1";
+	}
+	
+	// 자유 게시판 [미구현]
+	@RequestMapping(value = "/board/free", method = RequestMethod.GET)
+	public String goFreeboard() {
+		return "board/list";
 	}
 	
 }
