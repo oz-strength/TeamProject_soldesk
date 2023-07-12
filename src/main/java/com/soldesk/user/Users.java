1f56c4e1d5d586ab59211d8a1149e7130f7fd1bb
heewon <kimhw2030@naver.com>
2023-07-12 11:11:21 +0900

    back-end

---
+++ src/main/java/com/soldesk/user/Users.java
@@ -0,0 +1,26 @@
+package com.soldesk.user;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Users {
+	private List<User> user;
+	public Users() {
+		// TODO Auto-generated constructor stub
+	}
+	public Users(List<User> user) {
+		super();
+		this.user = user;
+	}
+	public List<User> getUser() {
+		return user;
+	}
+	@XmlElement
+	public void setUser(List<User> user) {
+		this.user = user;
+	}
+	
+}
