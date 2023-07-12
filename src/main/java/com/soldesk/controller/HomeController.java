773690c3c1459baed1d4b5dfb80c414b2a07f3c0
heewon <kimhw2030@naver.com>
2023-07-12 17:24:50 +0900

    홈 컨트롤러 footer 백엔드기능 추가

--- src/main/java/com/soldesk/controller/HomeController.java
+++ src/main/java/com/soldesk/controller/HomeController.java
@@ -10,31 +10,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 public class HomeController {
-	
+
 	// 초기화면
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String goHome() {
 		return "home";
 	}
-	
+
 	// 전국 지도 화면
 	@RequestMapping(value = "/mountain/map", method = RequestMethod.GET)
 	public String goMountain_map() {
 		return "mountain/map";
 	}
-	
+
 	// 마이 페이지 [미구현]
 	@RequestMapping(value = "/user/mypage", method = RequestMethod.GET)
 	public String goMypage() {
 		return "home";
 	}
-	
+
 	// 자유 게시판 [미구현]
 	@RequestMapping(value = "/board/free", method = RequestMethod.GET)
 	public String goFreeboard() {
 		return "board/list";
 	}
-	
+
+	/* footer 컨트롤러 - 시작 부분 */
+	// 개인정보 보호 및 약관
+	@RequestMapping(value = "/footer/privacy", method = RequestMethod.GET)
+	public String goPrivacy_and_Terms() {
+		return "home";
+	}
+	// 정보
+	@RequestMapping(value = "/footer/information", method = RequestMethod.GET)
+	public String goInformation() {
+		return "home";
+	}
+	// 서비스
+	@RequestMapping(value = "/footer/service", method = RequestMethod.GET)
+	public String goService() {
+		return "home";
+	}
+	/* footer 컨트롤러 - 끝 부분 */
+
 }
