0136da4562e8842734249cf6766295e6488e2455
heewon <kimhw2030@naver.com>
2023-07-19 12:08:46 +0900

    mountain 백엔드 추가

--- src/main/java/com/soldesk/entity/mountain/Mountain.java
+++ src/main/java/com/soldesk/entity/mountain/Mountain.java
@@ -1,43 +1,83 @@
 package com.soldesk.entity.mountain;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Mountain {
+	private int m_no;
 	private String m_name;
-	private String m_latitude;
-	private String m_longitude;
+	private int m_height;
+	private String m_location;
+	private String m_address;
+	private String m_photo;
 	
 	public Mountain() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Mountain(String m_name, String m_latitude, String m_longitude) {
+	public Mountain(int m_no, String m_name, int m_height, String m_location, String m_address, String m_photo) {
 		super();
+		this.m_no = m_no;
 		this.m_name = m_name;
-		this.m_latitude = m_latitude;
-		this.m_longitude = m_longitude;
+		this.m_height = m_height;
+		this.m_location = m_location;
+		this.m_address = m_address;
+		this.m_photo = m_photo;
+	}
+
+	public int getM_no() {
+		return m_no;
+	}
+
+	@XmlElement
+	public void setM_no(int m_no) {
+		this.m_no = m_no;
 	}
 
 	public String getM_name() {
 		return m_name;
 	}
 
+	@XmlElement
 	public void setM_name(String m_name) {
 		this.m_name = m_name;
 	}
 
-	public String getM_latitude() {
-		return m_latitude;
+	public int getM_height() {
+		return m_height;
 	}
 
-	public void setM_latitude(String m_latitude) {
-		this.m_latitude = m_latitude;
+	@XmlElement
+	public void setM_height(int m_height) {
+		this.m_height = m_height;
 	}
 
-	public String getM_longitude() {
-		return m_longitude;
+	public String getM_location() {
+		return m_location;
 	}
 
-	public void setM_longitude(String m_longitude) {
-		this.m_longitude = m_longitude;
+	@XmlElement
+	public void setM_location(String m_location) {
+		this.m_location = m_location;
+	}
+
+	public String getM_address() {
+		return m_address;
+	}
+
+	@XmlElement
+	public void setM_address(String m_address) {
+		this.m_address = m_address;
+	}
+
+	public String getM_photo() {
+		return m_photo;
+	}
+
+	@XmlElement
+	public void setM_photo(String m_photo) {
+		this.m_photo = m_photo;
 	}
 	
 	
